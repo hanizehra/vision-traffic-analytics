ef34dc1568c4134cf2456a8e0962dc63a2503ad5
Hany <hanizehrah@gmail.com>
2026-09-01 23:40:17 +0500

    corrected Path related errors

--- src/vision_traffic_analytics/config.py
+++ src/vision_traffic_analytics/config.py
@@ -2,8 +2,15 @@ from pathlib import Path
 
 VIDEO_PATHS = {
     
-    "c1" : Path("data/videos/vehicle/v1_one_way_road.mp4"),
-    "c2" : Path("data/videos/vehicle/v2_two_way_road.mp4"),
+    "v1" : Path("data/videos/vehicle/v1_one_way_road.mp4"),
+    "v2" : Path("data/videos/vehicle/v2_two_way_road.mp4"),
     "p1" : Path("data/videos/people/p1_one_way_entrance.mp4"),
     "p2" : Path("data/videos/people/p2_two_way_entrance.mp4"),
+}
+
+GROUND_TRUTH_PATHS = {
+    "v1": Path("data/ground_truth/vehicle/v1_one_way_road.json"),
+    "v2": Path("data/ground_truth/vehicle/v2_two_way_road.json"),
+    "p1": Path("data/ground_truth/people/p1_one_way_entrance.json"),
+    "p2": Path("data/ground_truth/people/p2_two_way_entrance.json"),
 }
